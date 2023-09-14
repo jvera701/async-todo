4f10ab5b3a31e9aca804dc56e845d68705348ba4
Jose Alejandro Vera <javera701@gmail.com>
2023-09-14 12:02:18 -0500

    fixing boiler

--- screens/detail/detail.tsx
+++ screens/detail/detail.tsx
@@ -1,7 +1,12 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
+import styles from "./detail.styles";
 
 const DetailScreen = () => {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <Text>{"This is a detail screen"}</Text>
+    </View>
+  );
 };
 
 export default DetailScreen;
